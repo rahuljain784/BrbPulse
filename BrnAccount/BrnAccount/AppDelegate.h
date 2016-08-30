141a0e32a7b12aa33977d7df0904e78ac1136b9e
Student <student@achiever69.local>
2016-08-30 09:13:13 +0530

    perform commit

---
+++ BrnAccount/BrnAccount/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrnAccount
+//
+//  Created by Student on 30/08/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BrnAccount/BrnAccount/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BrnAccount
+//
+//  Created by Student on 30/08/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
